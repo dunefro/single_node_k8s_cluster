c7b3c9cf28aca62bc41ce494bc70eaf561fd98c1
Vedant Pareek <36420365+dunefro@users.noreply.github.com>
2020-01-24 13:01:57 +0530

    Update kubeadm.sh

--- kubeadm.sh
+++ kubeadm.sh
@@ -52,3 +52,5 @@ echo -e "Deploying latest Helm package manager...\n"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+echo -e "Deployed Helm"
+helm version
