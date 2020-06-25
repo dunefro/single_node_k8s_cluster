76c593f68a55ee54375042aaa7fa980be3e2cf58
Vedant Pareek <dunefro@gmail.com>
2020-06-25 18:28:04 +0000

    Script echoes

--- del.sh
+++ del.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+echo -e "Deleting the node"
+
 sudo kubeadm reset --force
 
+echo -e "\n Restarting the docker daemon"
+
 sudo systemctl restart docker
+
+echo -e "\n Flushing the iptable rules"
 
 sudo iptables -F
 
